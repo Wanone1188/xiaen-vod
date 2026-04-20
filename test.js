a196978b4705b2268234686bdd660ffac86f56c4
Wanone1188 <wanone1202@gmail.com>
2026-04-21 04:51:45 +0800

    Add initial widget configuration in test.js

---
+++ test.js
@@ -0,0 +1,12 @@
+Widget({
+  id: "xiaen-test",
+  name: "夏恩自用合集",
+  author: "夏恩",
+  avatar: "https://raw.githubusercontent.com/Wanone1188/xiaen-vod/main/IMG_3225.jpeg",
+  version: "1.0",
+  description: "测试用",
+  items: [
+    "https://raw.githubusercontent.com/Wanone1188/xiaen-vod/main/1080%E8%B5%84%E6%BA%90.js",
+    "https://raw.githubusercontent.com/Wanone1188/xiaen-vod/main/155%E8%B5%84%E6%BA%90.js"
+  ]
+})
