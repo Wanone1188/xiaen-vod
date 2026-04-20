6fa8984724e4177d8a074e661a4d7c5f2d79a3be
Wanone1188 <wanone1202@gmail.com>
2026-04-21 04:54:10 +0800

    Create xiaen-home.js

---
+++ xiaen-home.js
@@ -0,0 +1,12 @@
+Widget({
+  id: "xiaen-home-2025",
+  name: "夏恩自用合集",
+  author: "夏恩",
+  avatar: "https://raw.githubusercontent.com/Wanone1188/xiaen-vod/main/IMG_3225.jpeg",
+  version: "1.0.0",
+  description: "个人影视资源导航",
+  items: [
+    "https://raw.githubusercontent.com/Wanone1188/xiaen-vod/main/1080%E8%B5%84%E6%BA%90.js",
+    "https://raw.githubusercontent.com/Wanone1188/xiaen-vod/main/155%E8%B5%84%E6%BA%90.js"
+  ]
+})
